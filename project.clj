5803b765482187116a5b97205bd59c81472368b3
dennis zhuang <killme2008@gmail.com>
2014-04-01 17:34:47 +0800

    Added project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject avos.data.json "0.2.5-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"]
+  :dependencies [[org.clojure/clojure "1.5.1"]])
